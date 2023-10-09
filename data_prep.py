b82b429ee285fa186b19aacf4a382d0fa49d5a40
danielAdama <adamadaniel321@gmail.com>
2023-10-10 00:34:47 +0100

    fix: bug

--- data_prep.py
+++ data_prep.py
@@ -4,8 +4,12 @@ import glob
 import cv2
 import os
 import random
+import argparse
+import logging
 
-# Define your class_id_mapping
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
 class_id_mapping = {
     '0': '1',
     '2': '1',
@@ -21,101 +25,8 @@ class_id_mapping = {
     '4': '6',
 }
 
-source_directory = os.path.join(os.getcwd(),"vehicles.v1")
-target_directory = os.path.join(os.getcwd(),'combined')
-os.makedirs(target_directory, exist_ok=True)
-
 def create_directories_if_not_exist(directory):
     os.makedirs(directory, exist_ok=True)
-
-
-def process_images(source_dir, target_dir):
-    total_images = 0
-    total_labels = 0
-
-    for dataset in ['train', 'test', 'valid']:
-        source_dataset_dir = os.path.join(source_dir, dataset)
-        target_dataset_dir = os.path.join(target_dir, dataset)
-        create_directories_if_not_exist(target_dataset_dir)
-        
-        for data_type in ['images', 'labels']:
-            source_data_dir = os.path.join(source_dataset_dir, data_type)
-            target_data_dir = os.path.join(target_dataset_dir, data_type)
-
-            create_directories_if_not_exist(target_dataset_dir)
-
-            image_paths = glob.glob(os.path.join(source_data_dir, '*.jpg'))            
-            label_paths = glob.glob(os.path.join(source_data_dir.replace('images', 'labels'), '*.txt'))
-
-            print(f"{dataset.upper()} Dataset - {data_type.capitalize()}:")
-            print(f"  Images: {len(image_paths)}")
-            print(f"  Labels: {len(label_paths)}")
-
-            if data_type == 'images':
-                total_images += len(image_paths)
-            elif data_type == 'labels':
-                total_labels += len(label_paths)
-
-
-            for image_path in image_paths:
-                base_filename = os.path.splitext(os.path.basename(image_path))[0]
-
-                # Load corresponding label file
-                label_file_path = os.path.join(source_data_dir.replace('images', 'labels'), base_filename + '.txt')
-                if not os.path.exists(label_file_path):
-                    continue
-
-                with open(label_file_path, 'r') as label_file:
-                    modified_data = []
-                    bboxes = []
-                    labels = []
-                    label_lines = label_file.readlines()
-
-                    for line in label_lines:
-                        parts = line.strip().split(' ')
-                        
-                        # Check if there are at least four values in the line
-                        if len(parts) >= 5:
-                            class_id = parts[0]
-                            bbox_str = ' '.join(parts[1:])
-                            
-                            x_ctr, y_ctr, w, h = bbox_str.split(' ')
-                            x_ctrf = float(x_ctr)
-                            y_ctrf = float(y_ctr)
-                            wf = float(w)
-                            hf = float(h)
-
-                            if class_id in class_id_mapping:
-                                label = class_id_mapping[class_id]
-
-                            bboxes.append([x_ctrf, y_ctrf, wf, hf])
-                            labels.append([label])
-
-                            modified_data.append((label, x_ctr, y_ctr, w, h))
-                        else:
-                            # Print information about the skipped line
-                            file_name = os.path.basename(label_file.name)
-                            num_objects = len(label_lines)
-                            print(f"Skipping line in {file_name}: class_id={class_id}, num_objects={num_objects}")
-                            print(f"Line content: {line.strip()}")
-                        
-                new_label_file_path = os.path.join(target_data_dir.replace('images', 'labels'), base_filename + '.txt')
-                os.makedirs(os.path.dirname(new_label_file_path), exist_ok=True)
-
-                with open(new_label_file_path, 'w') as new_label_file:
-                    for data in modified_data:
-                        label, x_ctr, y_ctr, w, h = data
-                        new_label_file.write(f"{label} {x_ctr} {y_ctr} {w} {h}\n")
-                
-                os.makedirs(target_data_dir, exist_ok=True)
-                new_image_path = os.path.join(target_data_dir, os.path.basename(image_path))
-                shutil.copy(image_path, new_image_path)
-
-    print("\nTotal ->")
-    print(f"  Images: {total_images} processed")
-    print(f"  Labels: {total_labels} processed")
-
-
 
 def yolo2tobox(bboxes):
     xmin, ymin = bboxes[0] - bboxes[2]/2, bboxes[1] - bboxes[3]/2
@@ -144,22 +55,115 @@ def plot_box(image, bboxes, label):
             image, 
             str(label[idx][0]),
             (xmin, ymin),
-            cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.FONT_HERSHEY_COMPLEX,
             1,
             (0, 255, 25),
-            2
+            1
         )
 
     return image
 
-def show_detections(image_paths, label_paths, num_samples:int = 4):
+def process_images(source_dir, target_dir):
+    total_images = 0
+    total_labels = 0
 
+    for dataset in ['train', 'test', 'valid']:
+        source_dataset_dir = os.path.join(source_dir, dataset)
+        target_dataset_dir = os.path.join(target_dir, dataset)
+        create_directories_if_not_exist(target_dataset_dir)
+
+        for data_type in ['images', 'labels']:
+            source_data_dir = os.path.join(source_dataset_dir, data_type)
+            target_data_dir = os.path.join(target_dataset_dir, data_type)
+            create_directories_if_not_exist(target_dataset_dir)
+
+            image_paths = glob.glob(os.path.join(source_data_dir, '*.jpg'))            
+            label_paths = glob.glob(os.path.join(source_data_dir.replace('images', 'labels'), '*.txt'))
+
+            logger.info(f"{dataset.upper()} Dataset - {data_type.capitalize()}:")
+            logger.info(f"  Images: {len(image_paths)}")
+            logger.info(f"  Labels: {len(label_paths)}")
+
+            if data_type == 'images':
+                total_images += len(image_paths)
+            elif data_type == 'labels':
+                total_labels += len(label_paths)
+
+            for image_path in image_paths:
+                base_filename = os.path.splitext(os.path.basename(image_path))[0]
+
+                # Load corresponding label file
+                label_file_path = os.path.join(source_data_dir.replace('images', 'labels'), base_filename + '.txt')
+                if not os.path.exists(label_file_path):
+                    continue
+
+                with open(label_file_path, 'r') as label_file:
+                    modified_data = []
+                    bboxes = []
+                    labels = []
+                    label_lines = label_file.readlines()
+
+                    for line in label_lines:
+                        parts = line.strip().split(' ')
+                        
+                        # Check if there are at least five values in the parts
+                        if len(parts) < 6:
+                            class_id = parts[0]
+                            bbox_str = ' '.join(parts[1:])
+                            
+                            x_ctr, y_ctr, w, h = bbox_str.split(' ')
+                            x_ctrf = float(x_ctr)
+                            y_ctrf = float(y_ctr)
+                            wf = float(w)
+                            hf = float(h)
+
+                            if class_id in class_id_mapping:
+                                label = class_id_mapping[class_id]
+
+                            bboxes.append([x_ctrf, y_ctrf, wf, hf])
+                            labels.append([label])
+
+                            modified_data.append((label, x_ctr, y_ctr, w, h))
+                        else:
+                            # Print information about the skipped line
+                            file_name = os.path.basename(label_file.name)
+                            num_objects = len(label_lines)
+                            logger.warning(f"Skipping line in {file_name}: class_id={class_id}, num_objects={num_objects}")
+                            logger.warning(f"Line content: {line.strip()}")
+                        
+                new_label_file_path = os.path.join(target_data_dir.replace('images', 'labels'), base_filename + '.txt')
+                os.makedirs(os.path.dirname(new_label_file_path), exist_ok=True)
+
+                if os.path.exists(new_label_file_path):
+                    # If it exists, open it in append mode
+                    with open(new_label_file_path, 'a') as new_label_file:
+                        for data in modified_data:
+                            label, x_ctr, y_ctr, w, h = data
+                            new_label_file.write(f"{label} {x_ctr} {y_ctr} {w} {h}\n")
+                else:
+                    # If it doesn't exist, create a new label file and write to it
+                    with open(new_label_file_path, 'w') as new_label_file:
+                        for data in modified_data:
+                            label, x_ctr, y_ctr, w, h = data
+                            new_label_file.write(f"{label} {x_ctr} {y_ctr} {w} {h}\n")
+                
+                os.makedirs(target_data_dir, exist_ok=True)
+                new_image_path = os.path.join(target_data_dir, os.path.basename(image_path))
+                shutil.copy(image_path, new_image_path)
+
+    logger.info("\nTotal ->")
+    logger.info(f"  Images: {total_images} processed")
+    logger.info(f"  Labels: {total_labels} processed")
+
+
+def show_detections(image_paths, label_paths):
+    num_samples = 4
     all_images = []
     all_images.extend(glob.glob(os.path.join(image_paths, '*.jpg')))
     all_images.extend(glob.glob(os.path.join(image_paths, '*.jpeg')))
     all_images.extend(glob.glob(os.path.join(image_paths, '*.JPG')))
 
-    all_images.sort()
+    random.shuffle(all_images)
 
     class_label = {
     '1':'bus',
@@ -176,7 +180,6 @@ def show_detections(image_paths, label_paths, num_samples:int = 4):
     '6':'vehicle'
     }
 
-    # num_samples = len(all_images)
     plt.figure(figsize=(15, 12))
     for i in range(num_samples):
         j = random.randint(0,num_samples-1)
@@ -203,7 +206,6 @@ def show_detections(image_paths, label_paths, num_samples:int = 4):
             bboxes.append([x_ctrf, y_ctrf, wf, hf])
             labels.append([label])
         
-        
         result_img =  plot_box(image, bboxes, labels)
         plt.subplot(2, 2, i+1)
         plt.imshow(result_img[:, :, ::-1])
@@ -212,10 +214,20 @@ def show_detections(image_paths, label_paths, num_samples:int = 4):
     plt.tight_layout()
     plt.show()
 
-# process_images(source_directory, target_directory)
+def main(args):
+    if args.mode == 'show-detections':
+        show_detections(
+        os.path.join(args.target_dir, 'train', 'images'),
+        os.path.join(args.target_dir, 'train', 'labels'),
+    )
+    if args.mode == 'prepare-detections':
+        process_images(args.source_dir, args.target_dir)
 
-show_detections(
-    os.path.join(os.getcwd(),'combined','train', 'images'),
-    os.path.join(os.getcwd(),'combined','train', 'labels'),
-    4
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process images and labels.")
+    parser.add_argument("--source-dir", type=str, required=True, help="Source directory containing the images and labels.")
+    parser.add_argument("--target-dir", type=str, required=True, help="Target directory to save processed images and labels.")
+    parser.add_argument('--mode', type=str, choices=['show-detections', 'prepare-detections'], default='prepare detections',
+                        help="Choose 'show detections' to display detections or 'prepare detections' to process images and labels.")
+    args = parser.parse_args()
+    main(args)
